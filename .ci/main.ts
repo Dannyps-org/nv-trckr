a2e87774c9f61213516e3b57f2bcd3de213ba7a5
Daniel Silva <mail@dannyps.net>
2023-10-25 11:33:41 +0100

    debug

--- .ci/main.ts
+++ .ci/main.ts
@@ -4,6 +4,25 @@ import { $ } from "zx";
 import { Octokit } from "@octokit/rest";
 import { getRequiredEnvVar } from "./lib.js";
 
+async function main() {
+    const GITHUB_TOKEN = getRequiredEnvVar("GITHUB_TOKEN");
+    const GITHUB_REPOSITORY = getRequiredEnvVar("GITHUB_REPOSITORY");
+    const octokit = new Octokit();
+
+    var chartVersionFileContent = (await $`cat chart-version.txt`).toString().trim();
+    let pullRequestVersion = await getVersionFromPullRequestsByLogin(octokit, GITHUB_REPOSITORY, "github-actions[bot]");
+    let backboneHelmChartVersion = await getVersionFromEnmeshedBackboneRepositoryHelmChart(octokit);
+
+    if (backboneHelmChartVersion !== chartVersionFileContent || backboneHelmChartVersion !== pullRequestVersion) {
+        // a new version is available and there is no PR for it, neither have we updated to it yet.
+
+        await deletePRsTargetingMain();
+        let featureBranchName = `upgrade-helm/${backboneHelmChartVersion}`;
+        await createFeatureBranch(featureBranchName, backboneHelmChartVersion);
+        await createPr(`Bump helm-chart version to ${backboneHelmChartVersion}`, "Created by bot", "main", featureBranchName);
+    }
+}
+
 /**
  * Creates a PR from {@link compare} to {@link base}.
  * @returns the created PR Number
@@ -12,7 +31,14 @@ async function createPr(title: string, body: string, base: string, compare: stri
     return 1;
 }
 
-async function getVersionFromPullRequestsByLogin(loginName: string): Promise<string> {
+/**
+ * 
+ * @param octokit the octokit instance to use
+ * @param GITHUB_REPOSITORY the owner/repo string of the current repo
+ * @param loginName the loginName of the author of the PRs to be considered 
+ * @returns the PR version
+ */
+async function getVersionFromPullRequestsByLogin(octokit: Octokit, GITHUB_REPOSITORY: string, loginName: string): Promise<string> {
     let [owner, repo] = GITHUB_REPOSITORY.split('/');
     let pulls = await octokit.rest.pulls.list({ owner, repo, state: "open" });
     let relevantPulls = pulls.data.filter(p => p.user?.login == loginName && p.title.match("v[0-9]+.[0-9]+.[0-9]+$") != null);
@@ -24,23 +50,35 @@ async function getVersionFromPullRequestsByLogin(loginName: string): Promise<str
     }
 }
 
-async function getVersionFromEnmeshedBackboneRepositoryHelmChart(): Promise<string> {
+/**
+ * 
+ * @param octokit the octokit instance to use
+ * @returns the helm chart version
+ */
+async function getVersionFromEnmeshedBackboneRepositoryHelmChart(octokit: Octokit): Promise<string> {
+    const tagPrefix = "helm/";
+
     let releases = await octokit.rest.repos.listReleases({ owner: "nmshd", repo: "backbone" });
-    console.log(releases.data[0])
-    let relevantReleases = releases.data.filter(p => p.tag_name?.startsWith("helm/"));
+    let relevantReleases = releases.data.filter(p => p.tag_name?.startsWith(tagPrefix));
     if (relevantReleases.length > 0) {
-        return relevantReleases[0].tag_name?.split('helm')[1] ?? 'no-release';
+        return relevantReleases[0].tag_name?.split(tagPrefix)[1] ?? 'no-release';
     } else {
         return 'no-release';
     }
 }
 
-const GITHUB_TOKEN = getRequiredEnvVar("GITHUB_TOKEN");
-const GITHUB_REPOSITORY = getRequiredEnvVar("GITHUB_REPOSITORY");
-const octokit = new Octokit();
+await main();
 
-var chartVersionFileContent = (await $`cat chart-version.txt`).toString().trim();
-let pullRequestVersion = await getVersionFromPullRequestsByLogin("github-actions[bot]");
-let backboneHelmChartVersion = await getVersionFromEnmeshedBackboneRepositoryHelmChart()
+async function deletePRsTargetingMain(): Promise<number> {
+    // to do
+    return 0;
+}
 
-console.warn(`file: ${chartVersionFileContent}, pr: ${pullRequestVersion}, backbone: ${backboneHelmChartVersion}`);
+async function createFeatureBranch(featureBranchName: string, backboneHelmChartVersion: string): Promise<void> {
+    await $`git config --global user.email "actions@github.com"`;
+    await $`git config --global user.name "GitHub Actions"`;
+    await $`echo ${backboneHelmChartVersion} > chart-version.txt`;
+    await $`git checkout -b ${featureBranchName} main`;
+    await $`git push --set-upstream origin ${featureBranchName}`;
+}
+
