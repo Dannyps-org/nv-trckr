a6c1389939a41ab14850d52631077f513f5ce396
Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
2023-10-31 07:29:28 +0000

    refactor: remove comments from version-tags.ts

--- .ci/version-tags.ts
+++ .ci/version-tags.ts
@@ -4,7 +4,7 @@ import { $ } from "zx";
 import { getRequiredEnvVar } from "./lib.js";
 
 await $`git fetch --tags --force`;
-let existingTags = (await $`git tag -l`).toString().split("\n").slice(0, -1);
+const existingTags = (await $`git tag -l`).toString().split("\n").slice(0, -1);
 
 const inputTag = getRequiredEnvVar("GIT_TAG");
 const nextTagVersion = getNextTagVersion(existingTags, inputTag);
@@ -19,13 +19,11 @@ await $`git push origin ${nextTagVersion} --force`;
 function getNextTagVersion(existingTags: string[], tag: string): string {
     // Find all tags with the specified name and extract their version numbers
     const matchingTags = existingTags
-        .filter((existingTag) => existingTag.startsWith(tag + "-"))
+        .filter((existingTag) => existingTag.startsWith(`${tag}-`))
         .map((existingTag) => parseInt(existingTag.split("-")[1]))
         .filter((versionNumber) => !isNaN(versionNumber));
 
-    // Determine the next version number
     const nextVersion = matchingTags.length > 0 ? Math.max(...matchingTags) + 1 : 1;
 
-    // Construct and return the next tag
     return `${tag}-${nextVersion}`;
 }
